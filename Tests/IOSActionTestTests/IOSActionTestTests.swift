ebdcad2f9b9f504f89e91e1aab451e3da6dc0da4
LiuEggy <161451396+liueggy@users.noreply.github.com>
2026-05-12 12:29:00 +0800

    Remove old Tests

--- Tests/IOSActionTestTests/IOSActionTestTests.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-@testable import IOSActionTest
-
-final class IOSActionTestTests: XCTestCase {
-    func testGreeting() {
-        let app = IOSActionTest()
-        XCTAssertTrue(app.greeting().contains("GitHub Actions"))
-    }
-}
