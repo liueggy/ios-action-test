cf61af49bf3acc9a4c01264a3b741ee51f4849ff
LiuEggy <161451396+liueggy@users.noreply.github.com>
2026-05-12 13:57:03 +0800

    Update package for UIKit Glass Tasks app

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 import PackageDescription
 
 let package = Package(
@@ -6,14 +6,17 @@ let package = Package(
     platforms: [
         .iOS(.v17)
     ],
+    products: [
+        .executable(name: "GlassTasks", targets: ["GlassTasks"])
+    ],
     targets: [
         .executableTarget(
             name: "GlassTasks",
             path: "App",
             linkerSettings: [
                 .linkedFramework("UIKit"),
-                .linkedFramework("SwiftUI"),
                 .linkedFramework("EventKit"),
+                .linkedFramework("Combine")
             ]
         )
     ]
